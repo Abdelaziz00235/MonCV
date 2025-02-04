11672de4788acab51f8ca2165062093295861f21
Abdoulaye Abdelaziz <abdoulayeabdelaziz94@gmail.com>
2025-02-04 11:46:19 +0100

    git

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site:'https://Abdelaziz00235.github.io',
+  base:'MonCV',
+  trailingSlash:'always',
   integrations: [tailwind()],
 });
